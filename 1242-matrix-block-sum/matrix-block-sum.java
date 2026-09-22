92cd4a2efafdc4670e0aac1b54cbd825d5558cfa
yogeeswarkoneru <yogeeswarkoneru13@gmail.com>
2026-09-22 12:25:00 +0530

    Time: 6 ms (96.43%) | Memory: 46.4 MB (90.25%) - LeetSync

---
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+       
+        int[][] pref = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pref[i + 1][j + 1] = mat[i][j] 
+                                  + pref[i][j + 1] 
+                                  + pref[i + 1][j] 
+                                  - pref[i][j];
+            }
+        }
+        
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int r1 = Math.max(0, i - k);
+                int c1 = Math.max(0, j - k);
+                int r2 = Math.min(m - 1, i + k);
+                int c2 = Math.min(n - 1, j + k);
+                
+                ans[i][j] = pref[r2 + 1][c2 + 1] 
+                          - pref[r1][c2 + 1] 
+                          - pref[r2 + 1][c1] 
+                          + pref[r1][c1];
+            }
+        }
+        
+        return ans;
+    }
+}
