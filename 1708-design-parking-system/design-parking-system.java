772d6fc8117e4b65f7256b556ffed8a03ea74aa8
yogeeswarkoneru <yogeeswarkoneru13@gmail.com>
2026-09-22 12:27:40 +0530

    Time: 8 ms (57.85%) | Memory: 47.1 MB (42.51%) - LeetSync

---
+++ 1708-design-parking-system/design-parking-system.java
@@ -0,0 +1,16 @@
+class ParkingSystem {
+    private int[] count;
+
+    public ParkingSystem(int big, int medium, int small) {
+        count = new int[] {0,big,medium,small};
+    }
+    
+    public boolean addCar(int carType) {
+        if(count[carType]>0){
+            count[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
+
